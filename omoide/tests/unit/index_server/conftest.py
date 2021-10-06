cd8e7e938a4c77558ab668a2904220f684a6f6ae
IgorZyktin <nicord@yandex.ru>
2021-10-06 21:57:42 +0300

    feat: improved test coverage on index server

---
+++ omoide/tests/unit/index_server/conftest.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""Tests.
+"""
+import datetime
+from unittest import mock
+
+import pytest
+
+from omoide import infra
+from omoide.index_server import search_engine, status
+from omoide.index_server import singleton
+
+
+@pytest.fixture
+def index_server_empty_index():
+    return search_engine.Index(all_metas=[], by_tags={})
+
+
+@pytest.fixture
+def index_server_index():
+    all_metas = [
+        search_engine.ShallowMeta('u1', 1, 'cat.jpg'),
+        search_engine.ShallowMeta('u2', 2, 'dog.jpg'),
+        search_engine.ShallowMeta('u3', 3, 'frog.jpg'),
+        search_engine.ShallowMeta('u4', 4, 'ant.jpg'),
+        search_engine.ShallowMeta('u5', 5, 'bird.jpg'),
+        search_engine.ShallowMeta('u6', 6, 'box.jpg'),
+        search_engine.ShallowMeta('u7', 7, 'building.jpg'),
+        search_engine.ShallowMeta('u8', 8, 'river.jpg'),
+        search_engine.ShallowMeta('u9', 9, 'sky.jpg'),
+    ]
+
+    by_tags = {
+        'animal': frozenset(['u1', 'u2', 'u3', 'u4', 'u5']),
+        'object': frozenset(['u6', 'u7', 'u8']),
+        'small': frozenset(['u3', 'u4']),
+        'big': frozenset(['u7', 'u8']),
+    }
+    return search_engine.Index(all_metas=all_metas, by_tags=by_tags)
+
+
+@pytest.fixture
+def fix_singleton(index_server_empty_index):
+    inst = singleton.Singleton()
+    inst.version = 'test'
+    inst.process = mock.Mock()
+    inst.process.memory_info.return_value = [0, 38476234, 0]
+    inst.filesystem = infra.Filesystem()
+    inst.stdout = infra.STDOut()
+    inst.status = status.Status(
+        datetime.datetime(2021, 10, 6, 21, 37).replace(
+            tzinfo=datetime.timezone.utc)
+    )
+    inst.index = index_server_empty_index
+    inst.db_path = '/test/path'
+    return inst
---
+++ omoide/tests/unit/index_server/test_basic_requests.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+"""Tests.
+"""
+import asyncio
+from unittest import mock
+from unittest.mock import patch
+
+from starlette.testclient import TestClient
+
+from omoide.index_server.app import app
+
+client = TestClient(app)
+
+
+def async_return(result):
+    f = asyncio.Future()
+    f.set_result(result)
+    return f
+
+
+def test_app_healthcheck():
+    # act
+    response = client.get('/')
+
+    # assert
+    assert response.status_code == 200
+    assert response.json() == {'result': 'ok'}
+
+
+def test_app_status(fix_singleton):
+    # act
+    with patch('omoide.index_server.app.logic.reload',
+               return_value=async_return('called')):
+        response = client.get('/status')
+
+    # assert
+    assert response.status_code == 200
+    assert response.json() == {
+        'index_avg_bucket': 0.0,
+        'index_buckets': 0,
+        'index_comment': '',
+        'index_last_reload': '2021-10-06 21:37:00+00:00',
+        'index_last_reload_duration': 0,
+        'index_max_bucket': 0,
+        'index_min_bucket': 0,
+        'index_records': 0,
+        'index_size': '0 B',
+        'index_status': 'init',
+        'index_uptime': '0s',
+        'server_last_restart': '2021-10-06 21:37:00+00:00',
+        'server_size': '0 B',
+        'server_uptime': '0s',
+        'version': 'test',
+    }
+
+
+def test_app_update_database_folder_read(fix_singleton):
+    # act
+    response = client.post('/update_database_folder', json={})
+
+    # assert
+    assert response.status_code == 200
+    assert response.json() == {'result': "Now using '/test/path'"}
+
+
+def test_app_update_database_folder_write(fix_singleton):
+    # act
+    response = client.post('/update_database_folder', json={'path': '/new'})
+
+    # assert
+    assert response.status_code == 200
+    assert response.json() == {'result': "Now using '/new'"}
+
+
+def test_app_reload(fix_singleton):
+    # act
+    with patch('omoide.index_server.app.logic') as fake_logic:
+        fake_logic.reload.return_value = async_return('called')
+        response = client.post('/reload')
+
+    # assert
+    assert response.status_code == 200
+    assert response.json() == {'result': 'Started reloading'}
+    fake_logic.reload.assert_called_once_with(fix_singleton)
+
+
+def test_app_search(fix_singleton):
+    # arrange
+    query = {'and_': [], 'or_': [], 'not_': [], 'page': 1, 'items_per_page': 2}
+    fake_return = mock.Mock()
+    fake_return.dict.return_value = {'ok': 1}
+
+    # act
+    with patch('omoide.index_server.app.logic') as fake_logic:
+        fake_logic.search.return_value = async_return(fake_return)
+        response = client.get('/search', json=query)
+
+    # assert
+    assert response.status_code == 200
+    assert response.json() == {'ok': 1}
+    fake_logic.search.assert_called_once_with(mock.ANY, fix_singleton)
