fd0b13196127191c42bae79eaacf3c679f86e5ab
IgorZyktin <nicord@yandex.ru>
2021-10-10 10:43:45 +0300

    feat: updated systemd files

---
+++ run_app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ./venv3.10/bin/activate
+python -m omoide run_app --root "/home/pi/OmoideStorage/root"
--- run_index.sh
+++ run_index.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ./venv/bin/activate
+source ./venv3.10/bin/activate
 python -m omoide run_index --root "/home/pi/OmoideStorage/root"
